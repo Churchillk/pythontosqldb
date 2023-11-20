1a996f8968acf515773c72390cbd38ffda0e5ac0
Churchillk <132873688+Churchillk@users.noreply.github.com>
2023-11-20 09:19:10 -0500

    update

--- fakertosql.py
+++ fakertosql.py
@@ -1,35 +1,55 @@
+from faker import Faker
 import mysql.connector
-def insert_data():
-    db_config = {
+import random
+from time import sleep
+
+db_config = {
         'host': 'localhost',
         'user': 'root',
         'password': '',
-        'database': 'CLASSA',
+        'database': 'NERDS',
     }
-    
-    # Sample data to be inserted
-    new_member = {
-        'CODE': '6',
-        'FNAME': 'John',
-        'LNAME': 'Doe',
-        'CONTACT': '+24412345678',
-        'AGE': '50',
-        'EMAIL': 'john.doe@example.com'
-    }
-    
-    try:
-        connection = mysql.connector.connect(**db_config)
-        cursor = connection.cursor()
-        insert_query = "INSERT INTO MEMBERS (CODE, FNAME, LNAME, CONTACT, AGE, EMAIL) VALUES (%s, %s, %s, %s, %s, %s)"
-        cursor.execute(insert_query, (new_member['CODE'], new_member['FNAME'], new_member['LNAME'], new_member['CONTACT'], new_member['AGE'], new_member['EMAIL']))
-        connection.commit()
-        print("Data inserted successfully!")
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
+data = []
+def generate_random_data(num_entries=10):
+    fake = Faker()
 
-    finally:
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
-insert_data()
+    value = 0
+    for _ in range(num_entries):
+        code = value
+        first_name = fake.first_name()
+        last_name = fake.last_name()
+        email = fake.email()
+        contact = fake.phone_number()
+        age = random.randint(1, 50)
+
+        new_member = {
+            'CODE': code,
+            'FNAME': first_name,
+            'LNAME': last_name,
+            'CONTACT': contact,
+            'AGE': age,
+            'EMAIL': email
+        }
+        try:
+            connection = mysql.connector.connect(**db_config)
+            print("connection established")
+            cursor = connection.cursor()
+            insert_query = "INSERT INTO `NERD MEMBERS` (CODE, FNAME, LNAME, CONTACT, AGE, EMAIL) VALUES (%s, %s, %s, %s, %s, %s)"
+            cursor.execute(insert_query, (new_member['CODE'], new_member['FNAME'], new_member['LNAME'], new_member['CONTACT'], new_member['AGE'], new_member['EMAIL']))
+            connection.commit()
+            
+        except mysql.connector.Error as err:
+            print(f"DB-CONNECTION Error: {err}")
+
+        finally:
+            if cursor:
+                cursor.close()
+            if connection:
+                connection.close()
+        value += 1
+        sleep(1)
+        print(f"inserted {code}, {first_name}, {last_name}, {contact}, {age}, {email}")
+    print("Data inserted successfully!")
+    
+if __name__ == "__main__":
+    random_data = generate_random_data(50)
